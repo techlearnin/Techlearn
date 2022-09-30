d3ede7dd96e831cc5665b03e0aacfcbf7e615098
techlearnin <114740762+techlearnin@users.noreply.github.com>
2022-09-30 16:59:15 +0530

    Code added in Github class

--- src/jUnit/GitHub.java
+++ src/jUnit/GitHub.java
@@ -2,18 +2,32 @@ package jUnit;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class GitHub {
+	WebDriver driver;
   @Test
-  public void googlesearch() {
+  public void googlesearch() throws InterruptedException {
+	  driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Selenium Training in Tirupati");
+		Thread.sleep(3000);
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
   }
   @BeforeTest
-  public void beforeTest() {
+  public void beforeTest() throws Exception {
+	  System.setProperty("webdriver.chrome.driver", "E:\\Lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
   }
 
   @AfterTest
   public void afterTest() {
+	 
   }
 
 }
